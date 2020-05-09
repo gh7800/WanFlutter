494f236bae180eb9541c38153e449fd1290b8c8b
wangshuai <bjwangs@163.com>
2020-05-09 13:06:48 +0800

    firstCommit

---
+++ wanflutter/android/app/src/main/java/com/shineiot/wanflutter/MainActivity.java
@@ -0,0 +1,13 @@
+package com.shineiot.wanflutter;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
